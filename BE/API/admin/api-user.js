5718c2ef823be6df4c9c98b5fdeaed6ece170361
ReDoProgrammer <redoprogrammer@gmail.com>
2021-03-01 10:00:55 +0700

    authentication with accesstoken

--- BE/API/admin/api-user.js
+++ BE/API/admin/api-user.js
@@ -4,41 +4,18 @@ const { authenticateAdminToken } = require("../../../middlewares/middleware");
 const UserGroup = require('../../models/user-group-model');
 const User = require('../../models/user-model');
 
+const jwt = require("jsonwebtoken");
 
-router.get('/',authenticateAdminToken,(req,res)=>{
-    let {page,limit,search} = req.query;
-    UserGroup.find({
-        $or:[
-            { name: { $regex: search, $options: "i" } },
-            { meta: { $regex: search, $options: "i" } },
-        ]
-    })
-    .sort({order:1})
-    .skip((page-1)*parseInt(limit))
-    .limit(parseInt(limit))
-    .select('name')
-    .exec((err,groups)=>{
-        if(err){
-            console.log('Load danh sách môn thất bại. Lỗi: '+err.message);
-            return res.status(500).json({
-                msg:'Load danh sách nhóm user thất bại!',
-                error:err.message
-            });
-        }
+let refershTokens = [];
 
-        return res.status(200).json({
-            msg:'Load danh sách nhóm người dùng thành công!',
-            groups:groups
-        });
-    });
-});
+
+
 
 
 router.post("/login", (req, res) => {
     let { username, password } = req.body;
   
-  console.log({ username, password });
-
+ 
     User.findOne({ username: username }, function (err, user) {
       if (err) {
         return res.status(500).json({
@@ -57,9 +34,10 @@ router.post("/login", (req, res) => {
             UserGroup.findOne({}, {}, { sort: { 'created_at' : -1 } }, function(err, group) {
               console.log(user.group,group._id);
               if(user.group.equals(group._id)){
+                console.log(user.group);
                   let u = {
                     _id: user._id,
-                    group:user.group
+                    group:user.group.name
                   };
                   const accessToken = generateAccessToken(u);
                   const refreshToken = jwt.sign(u, process.env.REFRESH_TOKEN_SECRET);
@@ -67,6 +45,7 @@ router.post("/login", (req, res) => {
                   refershTokens.push(refreshToken);
                   return res.status(200).json({
                     msg:'Đăng nhập thành công!',
+                    url:'/admin',
                     accessToken: accessToken,
                     refreshToken: refreshToken,
                   });
@@ -90,4 +69,8 @@ router.post("/login", (req, res) => {
     });
   });
 
+
+  function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "72h" });
+  }
   module.exports = router;
