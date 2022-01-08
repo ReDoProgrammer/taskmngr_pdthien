7dae010c4a0deff69542ab0eabd3302fb759a0c9
ReDoProgrammer <redoprogrammer@gmail.com>
2022-01-08 22:14:12 +0700

    define module model

---
+++ BE/models/module-model.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const moduleSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:Boolean,
+        default:true
+    }    
+    
+});
+
+
+module.exports = mongoose.model('module',moduleSchema);
+
+
+
