3881b3bc3f74c5b6c8153f218daa1236050b5e83
ReDo Programmer <redoprogrammer@gmail.com>
2022-02-10 09:43:40 +0700

    load qa and editor list and bind them into select controls

--- BE/API/TLA/api-tla-user.js
+++ BE/API/TLA/api-tla-user.js
@@ -1,32 +1,117 @@
 const router = require('express').Router();
-const { TLAMiddleware } = require("../../../middlewares/tla-middleware");
+const { authenticateTLAToken } = require("../../../middlewares/tla-middleware");
 const User = require('../../models/user-model');
+const UserModule = require('../../models/user-module-model');
+const Module = require('../../models/module-model');
+const _EDITOR = 'EDITOR';
+const _QA = 'QA';
 
 
-router.get('/list',(req,res)=>{  
-    User.find({
-        is_active:true,
-        
-    }) 
-    .select('fullname')   
-    .then(users=>{
-        return res.status(200).json({
-            msg:'Load users successfully!',
-            users:users
+/*
+    Hàm load danh sách Editor dựa vào module đã set quyền nhân viên
+    Chỉ load những nhân viên Editor có trạng thái hoạt động is_active:true
+*/
+router.get('/list-editor',authenticateTLAToken,(req,res)=>{  
+    getModuleId(_EDITOR)
+    .then(result=>{
+        UserModule
+        .find({module:result.m._id})
+        .exec()
+        .then(um=>{
+            let umList = um.map(x=>{
+                return x.user._id
+            });           
+
+            User
+            .find({_id:{$in:umList},is_active:true})
+            .select('fullname')
+            .exec()           
+            .then(editors=>{
+                return res.status(200).json({
+                    msg:`Load editors list successfully!`,
+                    editors
+                })
+            })
+            .catch(err=>{
+                return res.status(500).json({
+                    msg:`Can not load editors list with error: ${new Error(err.message)}`
+                })
+            })
         })
-    })
-    .catch(err=>{
-        console.log(new Error(`log users failed: {}`));
-
-        return res.status(500).json({
-            msg:'Load users list failed!',
-            error: new Error(err.message)
+        .catch(err=>{
+            return res.status(500).json({
+                msg:`Can not load user module with error: ${new Error(err.message)}`
+            })
         })
-    })
+    })     
+})
+
+/*
+    Hàm load danh sách Q.A dựa vào module đã set quyền nhân viên
+    Chỉ load những nhân viên Q.A có trạng thái hoạt động is_active:true
+*/
+
+router.get('/list-qa',authenticateTLAToken,(req,res)=>{  
+    getModuleId(_QA)
+    .then(result=>{
+        UserModule
+        .find({module:result.m._id})
+        .exec()
+        .then(um=>{
+            let umList = um.map(x=>{
+                return x.user._id
+            });           
+
+            User
+            .find({_id:{$in:umList},is_active:true})
+            .select('fullname')
+            .exec()           
+            .then(qas=>{
+                return res.status(200).json({
+                    msg:`Load Q.As list successfully!`,
+                    qas
+                })
+            })
+            .catch(err=>{
+                return res.status(500).json({
+                    msg:`Can not load Q.As list with error: ${new Error(err.message)}`
+                })
+            })
+        })
+        .catch(err=>{
+            return res.status(500).json({
+                msg:`Can not load user module with error: ${new Error(err.message)}`
+            })
+        })
+    })     
 })
 
 
 
 
-
 module.exports = router;
+
+const getModuleId = (moduleName)=>{
+    return new Promise((resolve,reject)=>{
+        Module.findOne({name:moduleName})
+        .exec()
+        .then(m=>{
+            if(!m){
+                return reject({
+                    code:404,
+                    msg:`Module not found`
+                })
+            }
+            return resolve({
+                msg:`Module found!`,
+                m
+            })
+        })
+        .catch(err=>{
+            return reject({
+                code:500,
+                msg:`Can not get module with error: ${new Error(err.message)}`
+            })
+        })
+    })
+}
