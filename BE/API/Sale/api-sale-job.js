d186d7eab8cc2a66fb22daff232a916db2eff356
ReDoProgrammer <redoprogrammer@gmail.com>
2022-02-15 17:05:41 +0700

    fix insert new job in sale module

--- BE/API/Sale/api-sale-job.js
+++ BE/API/Sale/api-sale-job.js
@@ -12,18 +12,18 @@ router.get("/list", authenticateSaleToken, (req, res) => {
     ]
   })
     .populate({
-      path:'customer',
-      
-        match:{
-          $or:[
-            { firstname: { "$regex": search, "$options": "i" } }
-          ]
-        }
-      
+      path: 'customer',
+
+      match: {
+        $or: [
+          { firstname: { "$regex": search, "$options": "i" } }
+        ]
+      }
+
     })
-   
+
     .exec()
-    .then((jobs) => {      
+    .then((jobs) => {
       let result = jobs.slice(process.env.PAGE_SIZE * (page - 1), process.env.PAGE_SIZE);
       console.log(result);
       return res.status(200).json({
@@ -51,91 +51,33 @@ router.post("/", authenticateSaleToken, (req, res) => {
     intruction,
   } = req.body;
 
-  console.log('job name: ', name);
-  //validation
-  if (source_link.trim().length == 0) {
-    return res.status(403).json({
-      msg: "Source link can not be blank",
-    });
-  }
-  if (received_date.trim().length == 0) {
-    return res.status(403).json({
-      msg: "Receive date can not be blank",
-    });
-  }
-  if (received_date.trim().length == 0) {
-    return res.status(403).json({
-      msg: "received date can not be blank",
-    });
-  }
 
-  if (delivery_date.trim().length == 0) {
-    return res.status(403).json({
-      msg: "delivery date can not be blank",
-    });
-  }
-
-  Promise.all([strToDate(received_date), strToDate(delivery_date)])
-    .then((result) => {
-      let d1 = result[0];
-      let d2 = result[1];
-      if (d1 >= d2) {
-        return res.status(403).json({
-          msg: "Receive date can not later than delivery date",
-        });
-      }
-      let job = new Job({
-        name,
-        customer,
-        source_link,
-        received_date: d1,
-        delivery_date: d2,
-        intruction,
+  let job = new Job({
+    name,
+    customer,
+    source_link,
+    received_date,
+    delivery_date,
+    intruction,
+  });
+  job
+    .save()
+    .then((j) => {
+      return res.status(201).json({
+        msg: "Create a new job successfully",
+        j
       });
-      job
-        .save()
-        .then((j) => {
-          return res.status(201).json({
-            msg: "Create a new job successfully",
-            job: j,
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          return res.status(500).json({
-            msg: "Cannot create new job",
-            error: new Error(err.message),
-          });
-        });
     })
     .catch((err) => {
-      return res.status(403).json({
-        msg: new Error(err.message),
+      console.log(err);
+      return res.status(500).json({
+        msg: "Cannot create new job",
+        error: new Error(err.message),
       });
     });
+
 });
 
 module.exports = router;
 
-var strToDate = (dtStr) => {
-  return new Promise((resolve, reject) => {
-    try {
-      let dateParts = dtStr.split("/");
-      let timeParts = dateParts[2].split(" ")[1].split(":");
-      dateParts[2] = dateParts[2].split(" ")[0];
-      return resolve(
-        new Date(
-          +dateParts[2],
-          dateParts[1] - 1,
-          +dateParts[0],
-          timeParts[0],
-          timeParts[1],
-          00
-        )
-      );
-    } catch (error) {
-      console.log(error);
-      return reject(new Error("Datetime format not match"));
-    }
-  });
-};
+
--- public/js/common.js
+++ public/js/common.js
@@ -1,32 +1,23 @@
-function strToDate(dtStr) {
-    if (!dtStr) return null
-    let dateParts = dtStr.split("/");
-    let timeParts = dateParts[2].split(" ")[1].split(":");
-    dateParts[2] = dateParts[2].split(" ")[0];
-    // month is 0-based, that's why we need dataParts[1] - 1
-    return dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0], timeParts[0], timeParts[1], timeParts[2]);
-  }
 
+function convertUTCDateToLocalDate(d) {
+  var date = convertUTCDateToLocalDate(new Date(d));
+  var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
 
-  function convertUTCDateToLocalDate(d) {
-    var date = convertUTCDateToLocalDate(new Date(d));
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+  var offset = date.getTimezoneOffset() / 60;
+  var hours = date.getHours();
 
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
+  newDate.setHours(hours - offset);
 
-    newDate.setHours(hours - offset);
-
-    return newDate;   
+  return newDate;
 }
 
 function convertISODateToTimeFormat(ISO_Date) {
   const newDateObj = new Date(ISO_Date);
   const toMonth = (newDateObj.getMonth() + 1);
-  const toMonthProcess = (toMonth<10?'0':'')+toMonth;
+  const toMonthProcess = (toMonth < 10 ? '0' : '') + toMonth;
   const toYear = newDateObj.getFullYear();
   const toDate = newDateObj.getDate();
-  const toDateProcess = (toDate<10?'0':'')+toDate;
+  const toDateProcess = (toDate < 10 ? '0' : '') + toDate;
 
 
   const toHours = newDateObj.getHours();
@@ -35,4 +26,36 @@ function convertISODateToTimeFormat(ISO_Date) {
   const toMinProcessed = (toMin < 10 ? '0' : '') + toMin;
   const dateTemplate = `${toDateProcess}/${toMonthProcess}/${toYear} ${toHoursProcessed}:${toMinProcessed}`;
   return dateTemplate;
+}
+
+function strToDate(dtStr) {
+  if (!dtStr) return null
+  try {
+    let dateParts = dtStr.split("/");
+    let timeParts = dateParts[2].split(" ")[1].split(":");
+    dateParts[2] = dateParts[2].split(" ")[0];
+    return  new Date(dateParts[2], dateParts[1] - 1, dateParts[0], timeParts[0], timeParts[1]);
+  } catch (error) {
+    return null;
+  }
+}
+
+function getCurrentTime() {
+  var currentdate = new Date();
+  return currentdate.getDate() + "/"
+    + (currentdate.getMonth() + 1) + "/"
+    + currentdate.getFullYear() + " "
+    + currentdate.getHours() + ":"
+    + currentdate.getMinutes();
+}
+
+function isValidHttpUrl(string) {
+  let url;  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
 }
