f7588d2c4db1eefc70d21120f41dae31137bb320
ReDoProgrammer <redoprogrammer@gmail.com>
2022-03-08 20:40:56 +0700

    show customer level list

--- BE/API/accountant/api-customer-level.js
+++ BE/API/accountant/api-customer-level.js
@@ -6,22 +6,22 @@ const { authenticateAccountantToken } = require("../../../middlewares/accountant
 router.get('/', authenticateAccountantToken, (req, res) => {
     let { customerId } = req.query;
 
-    // CustomerLevel
-    //     .find({ customer: customerId })
-    //     .populate('level')
-    //     .exec()
-    //     .then(cl => {
-    //         return res.status(200).json({
-    //             msg: 'Load customer levels successfully',
-    //             cl
-    //         })
-    //     })
-    //     .catch(err => {
-    //         console.log(`error found when loading customer levels: ${new Error(err.message)}`);
-    //         return res.status(500).json({
-    //             msg: `Can not load customer levels with error: ${new Error(err.message)}`
-    //         })
-    //     })
+    CustomerLevel
+        .find({ customer: customerId })
+        .populate('level')
+        .exec()
+        .then(cl => {
+            return res.status(200).json({
+                msg: 'Load customer levels successfully',
+                cl
+            })
+        })
+        .catch(err => {
+            console.log(`error found when loading customer levels: ${new Error(err.message)}`);
+            return res.status(500).json({
+                msg: `Can not load customer levels with error: ${new Error(err.message)}`
+            })
+        })
 })
 
 
