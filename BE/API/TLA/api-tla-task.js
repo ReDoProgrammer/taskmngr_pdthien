4ffb885d1c8b9b1d100b9e75c79f681a37786c2c
ReDo Programmer <redoprogrammer@gmail.com>
2022-03-10 09:38:16 +0700

    customize joblist view in TLA module

--- BE/API/TLA/api-tla-task.js
+++ BE/API/TLA/api-tla-task.js
@@ -185,67 +185,47 @@ router.post('/', authenticateTLAToken, (req, res) => {
         })
 })
 
-router.put('/', authenticateTLAToken, (req, res) => {
+router.put('/', authenticateTLAToken, async (req, res) => {
     let {
-        taskId,
-        level,
+        taskId,                   
         assigned_date,
         deadline,
         input_link,
         remark,
-        editor,
-        qa
+        qa_assigned,
+        editor_assigned,
+        qa,
+        editor
     } = req.body;
+    
+  
 
-    let task = Task.findById(taskId);
-    if (!task) {
-        return res.status(404).json({
-            msg: `Task not found!`
-        })
-    }
-
-
-    Task.findById(taskId)
-        .exec()
-        .then(t => {
-            if (!t) {
-                return res.status(404).json({
-                    msg: `Task not found`
-                })
-            }
-            Task
-                .findByIdAndUpdate(taskId, {
-                    level,
-                    assigned_date,
-                    deadline,
-                    input_link,
-                    remark,
-                    editor: (editor) ? editor : (t.editor),
-                    qa: (qa) ? qa : t.qa
-                }, { new: true }, (err, task) => {
-                    if (err) {
-                        return res.status(500).json({
-                            msg: `Can not update task by id with error: ${new Error(err.message)}`
-                        })
-                    }
-
-                    if (!task) {
-                        return res.status(404).json({
-                            msg: `Task not found!`
-                        })
-                    }
-                    return res.status(200).json({
-                        msg: `Update task successfully!`,
-                        task
-                    })
-                })
-        })
-        .catch(err => {
+    await Task.findByIdAndUpdate(taskId,{
+        assigned_date,
+        deadline,
+        input_link,
+        remark,
+        qa_assigned,
+        editor_assigned,
+        qa:(qa_assigned=='true'?qa:null),
+        editor:(editor_assigned=='true'?editor:null),
+        status:(editor_assigned=='true'?0:-1)
+    },{new:true},(err,task)=>{
+        if(err){
             return res.status(500).json({
-                msg: `Can not get task by id with error: ${new Error(err.message)}`
+                msg:`Can not update task with caught error: ${new Error(err.message)}`
             })
+        }
+        if(!task){
+            return res.status(404).json({
+                msg:`Task not found!`
+            })
+        }
+        return res.status(200).json({
+            msg:`The task has been updated!`,
+            task
         })
-
+    })
 
 })
 
