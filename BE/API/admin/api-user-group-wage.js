db0be4743564dedf639acdb8fb7a234668dbbc4e
ReDo Programmer <redoprogrammer@gmail.com>
2022-03-20 11:57:35 +0700

    show input link of job in job detailmodal in sale module

--- BE/API/admin/api-user-group-wage.js
+++ BE/API/admin/api-user-group-wage.js
@@ -138,7 +138,8 @@ router.get('/', authenticateAdminToken, (req, res) => {
         .populate('staff_lv', 'name -_id')
         .populate('module', 'name -_id')
         .exec()
-        .then(wages => {          
+        .then(wages => {     
+            console.log(wages);     
             return res.status(200).json({
                 msg: 'Load user group wages successfully',
                 wages
