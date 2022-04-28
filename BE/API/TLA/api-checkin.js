622a9f8d3440c209458746827307e7cf5cc60778
ReDo Programmer <redoprogrammer@gmail.com>
2022-04-28 10:37:14 +0700

    set checkin and checkout into staffs array

--- BE/API/TLA/api-checkin.js
+++ BE/API/TLA/api-checkin.js
@@ -5,6 +5,23 @@ const User = require('../../models/user-model');
 const Module = require('../../models/module-model');
 const UserModule = require('../../models/user-module-model');
 
+
+router.put('/out', authenticateTLAToken, (req, res) => {
+    let { checkOut } = req.body;
+    checkOut.forEach(async u => {
+        let chk = await CheckIn.findOne({staff:u});      
+        console.log(chk.check[chk.check.length - 1])
+        chk.check[chk.check.length - 1] = {
+            in:  chk.check[chk.check.length - 1].in,
+            out: new Date()
+        }
+        await chk.save();
+    })
+    return res.status(200).json({
+        msg: `Set checkout into staffs successfully!`
+    })
+})
+
 router.put('/in', authenticateTLAToken, (req, res) => {
     let { checkIn } = req.body;
     console.log(checkIn)
@@ -39,9 +56,6 @@ router.get('/list-staffs-by-module', authenticateTLAToken, (req, res) => {
                             let checkIn = [];
                             checkOut = [];
 
-
-                            console.log(users, staffs)
-
                             if (staffs.length == 0) {
                                 checkOut = users;
                                 return res.status(200).json({
@@ -51,11 +65,21 @@ router.get('/list-staffs-by-module', authenticateTLAToken, (req, res) => {
                                 })
                             }
 
+                            Promise.all([GetCheckInList(users)])
 
-
-
-
-
+                                .then(rs => {
+                                    checkIn = rs[0];
+                                    return res.status(200).json({
+                                        msg: `Load checkin & checkOut list successfully!`,
+                                        checkIn,
+                                        checkOut
+                                    })
+                                })
+                                .catch(err => {
+                                    return res.status(err.code).json({
+                                        msg: err.msg
+                                    })
+                                })
                         })
                         .catch(err => {
                             console.log(`Can not get checkin staffs with error: ${new Error(err.message)}`);
@@ -95,6 +119,23 @@ router.get('/list-modules', authenticateTLAToken, (req, res) => {
 
 
 module.exports = router;
+
+const GetCheckInList = (users) => {
+    return new Promise((resolve, reject) => {
+        CheckIn
+            .find({ 'check.out': null })
+            .populate('staff', 'username fullname')
+            .then(ci => {
+                return resolve(ci.map(x => x.staff));
+            })
+            .catch(err => {
+                return reject({
+                    code: 500,
+                    msg: `Can not load checkin list with err: ${new Error(err.message)}`
+                })
+            })
+    })
+}
 
 const GetUsersByModule = (moduleId) => {
     return new Promise((resolve, reject) => {
