9660495da9945ddd941765ff3b9cc2f8532b9014
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-03 13:43:34 +0700

    show top 10 job based on customer in sale module

--- BE/API/Sale/api-sale-job.js
+++ BE/API/Sale/api-sale-job.js
@@ -86,7 +86,6 @@ router.get('/list-by-customer',authenticateSaleToken,async (req,res)=>{
   .populate('created.by')
   .sort({_id: -1})
   .limit(10);
-  console.log(jobs)
   return res.status(200).json({
     msg:`Load jobs list by customer successfully!`,
     jobs
