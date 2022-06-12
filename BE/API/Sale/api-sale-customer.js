d6710dea1b3ac970fe59562642a46f0ef5878e48
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-12 15:12:20 +0700

    search customer in sale module

--- BE/API/Sale/api-sale-customer.js
+++ BE/API/Sale/api-sale-customer.js
@@ -1,38 +1,45 @@
 const router = require('express').Router();
 const { authenticateSaleToken } = require("../../../middlewares/sale-middleware");
 const Customer = require('../../models/customer-model');
+const pageSize = 20;
 
+router.get('/list',authenticateSaleToken,async (req,res)=>{
+    let { search, page } = req.query;
 
-router.get('/list',authenticateSaleToken,(req,res)=>{
-    let {page,search} = req.query;   
-    Customer.find({$or:[
-        {firstname:{ "$regex": search, "$options": "i" }},
-        {lastname:{ "$regex": search, "$options": "i" }},
-        {phone:{ "$regex": search, "$options": "i" }},
-        {email:{ "$regex": search, "$options": "i" }},
-    ]})
-    .populate('levels','name -_id')
-    .populate('output','name -_id')
-    .populate('size','name -_id')
-    .populate('color','name -_id')
-    .populate('cloud','name -_id')
-
-    .populate('nation','name -_id')
-    .exec()
-    .then(customers=>{        
-        let result = customers.slice(process.env.PAGE_SIZE*(page-1),process.env.PAGE_SIZE);   
-        return res.status(200).json({
-          msg:'Load customers successfully!',
-          pages:customers.length%process.env.PAGE_SIZE==0?customers.length/process.env.PAGE_SIZE:Math.floor(customers.length/process.env.PAGE_SIZE)+1,
-          customers:result
-        })
+    let customers = await Customer.find({
+        $or: [
+            { "name.firstname": { "$regex": search, "$options": "i" } },
+            { "name.lastname": { "$regex": search, "$options": "i" } },
+            { "contact.email": { "$regex": search, "$options": "i" } },
+            { "contact.phone": { "$regex": search, "$options": "i" } },
+        ]
+      
     })
-    .catch(err=>{
-        return res.status(500).json({
-            msg:'Can not load customers list!',
-            error: new Error(err.message)
-        })
-    })
+
+        .populate('contracts.lines.root')
+        .populate('contracts.lines.parents')
+        .sort({ '_id': 1 })
+        .select('name.firstname name.lastname contact.phone contact.email status jobs')
+        .skip((page - 1) * pageSize)
+        .limit(pageSize);
+
+    let count = await Customer.countDocuments({
+        $or: [
+            { "name.firstname": { "$regex": search, "$options": "i" } },
+            { "name.lastname": { "$regex": search, "$options": "i" } },
+            { "contact.email": { "$regex": search, "$options": "i" } },
+            { "contact.phone": { "$regex": search, "$options": "i" } },
+        ]
+        
+    });
+
+    return res.status(200).json({
+        msg: `Load customers list successfully!`,
+        customers,
+        pages: count % pageSize == 0 ? count / pageSize : Math.floor(count / pageSize) + 1,
+        pageSize
+    });
+   
 })
 
 router.get('/detail',(req,res)=>{
