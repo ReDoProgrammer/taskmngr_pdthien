1aa696ca00981d209ccae72a8324ab1b71215334
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-16 23:32:06 +0700

    load template as contract. not done

--- BE/API/Sale/api-template.js
+++ BE/API/Sale/api-template.js
@@ -3,11 +3,7 @@ const { authenticateSaleToken } = require("../../../middlewares/sale-middleware"
 const Template = require('../../models/template-model');
 
 router.get('/',authenticateSaleToken,async (req,res)=>{
-    let templates = await Template.find({});
-    return res.status(200).json({
-        msg:`Load templates list successfully!`,
-        templates
-    })
+    
 })
 
 module.exports = router;
