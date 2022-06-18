5ea32be845428618b16f3a24fe99180ffaab594b
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-18 11:09:33 +0700

    CRUD bonus penalty

--- public/js/common.js
+++ public/js/common.js
@@ -80,3 +80,7 @@ function copyToClipboard(containerid) {
   window.getSelection().removeAllRanges();
 
 }
+
+function kFormatter(num) {
+  return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'k' : Math.sign(num)*Math.abs(num)
+}
