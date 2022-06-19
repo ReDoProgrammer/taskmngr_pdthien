9309a6a9c45ae5749407faed912889b9080aba57
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-20 00:36:50 +0700

    fix load editor and qa list based on local job level in TLA module

--- BE/API/TLA/api-tla-user.js
+++ BE/API/TLA/api-tla-user.js
@@ -3,6 +3,7 @@ const { authenticateTLAToken } = require("../../../middlewares/tla-middleware");
 const User = require('../../models/user-model');
 const Group = require('../../models/user-group-model');
 const Module = require('../../models/module-model');
+const StaffLevel = require('../../models/staff-level-model');
 const { ObjectId } = require('mongodb');
 const { getModule } = require('../../../middlewares/common');
 
@@ -13,19 +14,34 @@ const _QA = 'QA';
 
 router.get('/list-editor', authenticateTLAToken, async (req, res) => {
     let { levelId } = req.query;
-   
-    getModule(_EDITOR)
-    .then(async m=>{
-        let groups = await Group.findOne({
-            'wages.job_lv':levelId,
-            'wages.module':m._id
-        }).populate('users');
-        console.log(groups)
-      
+
+    Promise.all([getModule(_EDITOR),GetStaffLevelsFromLocalJobLevel(levelId)])
+    .then(async rs=>{
+        let module = rs[0];
+        let staffLevels = rs[1].map(x=>x._id);
+
+        let groups = (await Group.find({
+            'wages.module':module,
+            'wages.staff_lv':{$in:staffLevels},
+            'wages.job_lv':levelId
+        })).map(x=>x._id);
+       
+        let editors = await User.find({
+            user_level:{$in:staffLevels},
+            user_group:{$in:groups},
+            _id: {$in:module.users}
+        }).select('fullname username');
+
+        return res.status(200).json({
+            msg:`Load editors successfully!`,
+            editors
+        })
     })
-   
-
-
+    .catch(err=>{
+        return res.status(err.code).json({
+            msg:err.msg
+        })
+    })
 })
 
 /*
@@ -37,8 +53,34 @@ router.get('/list-editor', authenticateTLAToken, async (req, res) => {
 router.get('/list-qa', authenticateTLAToken, async (req, res) => {
 
     let { levelId } = req.query;
+    Promise.all([getModule(_QA),GetStaffLevelsFromLocalJobLevel(levelId)])
+    .then(async rs=>{
+        let module = rs[0];
+        let staffLevels = rs[1].map(x=>x._id);
 
-  
+        let groups = (await Group.find({
+            'wages.module':module,
+            'wages.staff_lv':{$in:staffLevels},
+            'wages.job_lv':levelId
+        })).map(x=>x._id);
+       
+        let qas = await User.find({
+            user_level:{$in:staffLevels},
+            user_group:{$in:groups},
+            _id: {$in:module.users}
+        }).select('fullname username');
+
+        return res.status(200).json({
+            msg:`Load Q.A list successfully!`,
+            qas
+        })
+    })
+    .catch(err=>{
+        return res.status(err.code).json({
+            msg:err.msg
+        })
+    })
+
 
 })
 
@@ -49,3 +91,15 @@ module.exports = router;
 
 
 
+const GetStaffLevelsFromLocalJobLevel = levelId=>{
+    return new Promise(async (resolve,reject)=>{
+        let levels = await StaffLevel.find({levels:levelId});
+        if(levels.length == 0){
+            return reject({
+                code:404,
+                msg:`No staff level can access this job level`
+            })
+        }
+        return resolve(levels);
+    })
+}
