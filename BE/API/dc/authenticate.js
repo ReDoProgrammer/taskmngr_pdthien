a3a8d4a7a194429a72d533edaea7d75737a26917
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-25 22:32:15 +0700

    fix DC authentication

--- BE/API/dc/authenticate.js
+++ BE/API/dc/authenticate.js
@@ -5,7 +5,6 @@ const { authenticateDCToken } = require("../../../middlewares/dc-middleware");
 const _MODULE = 'DC';
 const {
   generateAccessToken,
-  getRole,
   getModule,
   checkAccount
 } = require('../common')
@@ -13,66 +12,54 @@ const {
 
 let refershTokens = [];
 
-router.get('/profile',authenticateDCToken,(req,res)=>{
-  User
-  .findById(req.user._id)
-  .exec()
-  .then(user=>{
-    if(!user){
-      return res.status(404).json({
-        msg:`User not found!`
-      })      
-    }
-    return res.status(200).json({
-      msg:`Get user profile successfully!`,
-      fullname: user.fullname
+router.get('/profile',authenticateDCToken,async (req,res)=>{
+  let user = await User.findById(req.user._id);
+  if (!user) {
+    return res.status(404).json({
+      msg: `User not found!`,
+      url: '/dc/login'
     })
-  })
-  .catch(err=>{
-    return res.status(500).json({
-      msg:`Can not get user profile with error: ${new Error(err.message)}`
-    })
+  }
+
+  return res.status(200).json({
+    msg: `Load user profile successfully!`,
+    fullname: user.fullname
   })
 })
 
 
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   let { username, password } = req.body;
   Promise.all([getModule(_MODULE), checkAccount(username, password)])
-    .then(result => {
-      getRole(result[0]._id, result[1]._id)
-        .then(chk => {        
-          if (chk) {
-            let user = result[1];
-            let u = {
-              _id: user._id           
-            };
-
-            const accessToken = generateAccessToken(u);
-            const refreshToken = jwt.sign(u, process.env.REFRESH_TOKEN_SECRET);
-
-            refershTokens.push(refreshToken);
-            return res.status(200).json({
-              msg: 'Login successfully!',
-              url: '/dc',
-              accessToken: accessToken,
-              refreshToken: refreshToken
-            });
-
-          }
-
+    .then(rs => {
+      if (!rs[0].users.includes(rs[1]._id)) {
+        return res.status(403).json({
+          msg: `You can not access this module!`,
+          url: '/dc/login'
         })
-        .catch(err => {
-          console.log(err);
-          return res.status(err.code).json({
-            msg: err.message
-          })
-        })
+      }
+      let u = {
+        _id: rs[1]._id
+      };
+
+      const accessToken = generateAccessToken(u);
+      const refreshToken = jwt.sign(u, process.env.REFRESH_TOKEN_SECRET);
+
+      refershTokens.push(refreshToken);
+      return res.status(200).json({
+        msg: 'Login successfully!',
+        url: '/dc',
+        accessToken: accessToken,
+        refreshToken: refreshToken
+      });
+
     })
     .catch(err => {
+      console.log(err);
       return res.status(err.code).json({
-        msg: `${new Error(err.msg)}`
+        msg: `${new Error(err.msg)}`,
+        url: '/dc/login'
       })
     })
 })
--- middlewares/dc-middleware.js
+++ middlewares/dc-middleware.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const _MODULE = 'DC';
-const {getModuleId} = require('./common');
+const { getModule } = require('./common');
 
 function authenticateDCToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.status(401).json({
-    msg: `Lỗi xác thực tài khoản. token null`
+    msg: `Token null`,
+    url: '/dc/login'
   });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
@@ -15,23 +16,30 @@ function authenticateDCToken(req, res, next) {
       console.log('got an error when veryfy account: ', new Error(err.message));
 
       return res.status(403).json({
-        msg: `Lỗi xác thực tài khoản ${err.message}`
+        msg: `Can not authenticate this account with error:  ${err.message}`
       });
 
     }
 
-    getModuleId(_MODULE)
-      .then(result => {       
-        
-
+    getModule(_MODULE)
+      .then(m => {
+        if (m.users.includes(user._id)) {
+          req.user = user;
+          next();
+        } else {
+          return res.status(403).json({
+            msg: `You can not access this module!`,
+            url: '/dc/login'
+          })
+        }
       })
       .catch(err => {
         console.log(err);
         return res.status(err.code).json({
-          msg: err.msg
+          msg: err.msg,
+          url: '/dc/login'
         })
       })
-
   });
 }
 
