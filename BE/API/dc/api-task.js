a8c2f75ff0057b54cf0f89e25aca3eae72ab643d
ReDo Programmer <redoprogrammer@gmail.com>
2022-06-25 23:35:11 +0700

    get task in DC module

--- BE/API/dc/api-task.js
+++ BE/API/dc/api-task.js
@@ -3,12 +3,15 @@ const Task = require("../../models/task-model");
 const { authenticateDCToken } = require("../../../middlewares/dc-middleware");
 const {
     GetTask,
-    GetCustomerById } = require('../common');
+    GetCustomerById,
+    getWage
+} = require('../common');
+const { ValidateCheckIn } = require('../../../middlewares/checkin-middleware');
 
 const _MODULE = 'DC';
 const pageSize = 20;
 
-router.put('/mark', authenticateDCToken, async (req, res) => {
+router.put('/mark', [authenticateDCToken, ValidateCheckIn], async (req, res) => {
     let { taskId, bpId, remark } = req.body;
     let task = await Task.findById(taskId);
     if (!task) {
@@ -53,175 +56,193 @@ router.put('/mark', authenticateDCToken, async (req, res) => {
 
 })
 
-router.put('/submit', authenticateDCToken, async(req, res) => {
+router.put('/submit', [authenticateDCToken, ValidateCheckIn], async (req, res) => {
     let { taskId } = req.body;
 
     //validate asif the task has been submited before
     let task = await Task.findById(taskId);
-    if(!task){
+    if (!task) {
         return res.status(404).json({
-            msg:`Task not found!`
+            msg: `Task not found!`
         })
     }
 
-    let dc = task.dc.filter(x=>x.staff == req.user._id && !x.unregisted && !x.submited_at);
+    let dc = task.dc.filter(x => x.staff == req.user._id && !x.unregisted && !x.submited_at);
     dc[0].submited_at = new Date();
     task.status = 3;
-    
+
     await task.save()
-    .then(_=>{
-        return res.status(200).json({
-            msg:`The task has been submited!`
+        .then(_ => {
+            return res.status(200).json({
+                msg: `The task has been submited!`
+            })
         })
-    })
-    .catch(err=>{
-        return res.status(500).json({
-            msg:`This task can not be submited with error: ${new Error(err.message)}`
+        .catch(err => {
+            return res.status(500).json({
+                msg: `This task can not be submited with error: ${new Error(err.message)}`
+            })
         })
-    })
 
 
 })
 
-router.put('/unregister', authenticateDCToken, async (req, res) => {
+router.put('/unregister', [authenticateDCToken, ValidateCheckIn], async (req, res) => {
     let { taskId } = req.body;
 
     let task = await Task.findById(taskId);
-    if(!task){
+    if (!task) {
         return res.status(404).json({
-            msg:`Task not found!`
+            msg: `Task not found!`
         })
     }
 
-    
 
-    let dc = task.dc.filter(x=>x.staff == req.user._id && !x.unregisted);
-    if(dc.length == 0){
+
+    let dc = task.dc.filter(x => x.staff == req.user._id && !x.unregisted);
+    if (dc.length == 0) {
         return res.status(404).json({
-            msg:`DC not found!`
+            msg: `DC not found!`
         })
     }
 
-    dc[dc.length-1].unregisted = true;
+    dc[dc.length - 1].unregisted = true;
     await task.save()
-    .then(_=>{
-        return res.status(200).json({
-            msg:`You have unregister this task successfully!`
+        .then(_ => {
+            return res.status(200).json({
+                msg: `You have unregister this task successfully!`
+            })
         })
-    })
-    .catch(err=>{
-        return res.status(500).json({
-            msg:`You can not unregister this task with error: ${new Error(err.message)}`
+        .catch(err => {
+            return res.status(500).json({
+                msg: `You can not unregister this task with error: ${new Error(err.message)}`
+            })
         })
-    })
 })
 
 
-router.put('/get-task', authenticateDCToken,async (req, res) => {
+router.put('/get-task', [authenticateDCToken, ValidateCheckIn], async (req, res) => {
     let { taskId } = req.body;
     let task = await Task.findById(taskId);
 
-    if(!task){
+    if (!task) {
         return res.status(404).json({
-            msg:`Task not found!`
+            msg: `Task not found!`
         })
     }
 
-    let chk = task.dc.filter(x=>x.staff != req.user._id && x.unregisted);
-    
-    if(chk.length > 0){
+    //Nếu task đã được submit trước đó
+    if (task.status == 3) {
         return res.status(403).json({
-            msg:`This task has been registed by another DC!`
+            msg: `This task have been already submited by another DC!`
         })
     }
-    await getModule(_MODULE)
-    .then(async module=>{        
-        await getWage(req.user._id,task.basic.level._id,module._id)
-        .then(async w=>{
+
+    /*
+        DC muốn nhận task thì phải đợi ít nhất đc Editor submit
+        Không nhất thiết phải được Q.A submit
+        Tuy nhiên nếu có Q.A thì phải đợi Q.A submit thì DC mới nhận được task
+    */
+    if ((task.qa.length == 0 && task.status < 1) || (task.qa.length > 0 && task.status < 2)) {
+        return res.status(403).json({
+            msg: `Task status is invalid. Please wait Editor/QA submits it!`
+        })
+    }
+
+
+    //Nếu task đã được DC khác nhận
+    if (task.dc.length > 0 && task.dc[task.dc.length - 1].staff != req.user._id && !task.dc[task.dc.length - 1].unregisted) {
+        return res.status(403).json({
+            msg: `This task has been registed by another DC!`
+        })
+    }
+
+     getWage(req.user._id, _MODULE, task.basic.level.toString())
+        .then(async wage => {           
             task.dc.push({
-                staff:req.user._id,
+                staff: req.user._id,
                 got_at: new Date(),
-                wage: w.wage
+                wage
             });
             await task.save()
-            .then(_=>{
-                return res.status(200).json({
-                    msg:`You have registed this task successfully!`
+                .then(_ => {
+                    return res.status(200).json({
+                        msg: `You have registed this task successfully!`
+                    })
                 })
-            })
-            .catch(err=>{
-                return res.status(500).json({
-                    msg:`Can not get task with error: ${new Error(err.message)}`
+                .catch(err => {
+                    return res.status(500).json({
+                        msg: `Can not get task with error: ${new Error(err.message)}`
+                    })
                 })
+        })
+        .catch(err=>{
+            console.log(err)
+            return res.status(err.code).json({
+                msg:err.msg
             })
         })
-    })
-    .catch(err=>{
-        return res.status(err.code).json({
-            msg:err.msg
-        })
-    })
-   
-  
-   
-    
+
+
+
+
+
 })
 
-router.put('/reject', authenticateDCToken, async (req, res) => {
+router.put('/reject', [authenticateDCToken, ValidateCheckIn], async (req, res) => {
     let { taskId, remark } = req.body;
 
     let task = await Task.findById(taskId);
 
-    if(!task){
+    if (!task) {
         return res.status(404).json({
-            msg:`Task not found!`
+            msg: `Task not found!`
         })
     }
 
-    if(task.dc.length==0){
+    if (task.dc.length == 0) {
         return res.status(404).json({
-            msg:`DC not found!`
+            msg: `DC not found!`
         })
     }
 
     let rm = new Remark({
         user: req.user._id,
         content: remark,
-        tid:taskId
+        tid: taskId
     });
 
     await rm.save()
-    .then(async _=>{
-        task.remarks.push(rm);
-        task.dc[task.dc.length-1].rejected.push({
-            at:new Date(),
-            by:req.user._id,
-            rm:rm._id
-        });
-        task.status = -3;
+        .then(async _ => {
+            task.remarks.push(rm);
+            task.dc[task.dc.length - 1].rejected.push({
+                at: new Date(),
+                by: req.user._id,
+                rm: rm._id
+            });
+            task.status = -3;
 
-        await task.save()
-        .then(_=>{
-            return res.status(200).json({
-                msg:`Task has been rejected!`
-            })
+            await task.save()
+                .then(_ => {
+                    return res.status(200).json({
+                        msg: `Task has been rejected!`
+                    })
+                })
+                .catch(err => {
+                    return res.status(500).json({
+                        msg: `Can not reject this task with error: ${new Error(err.message)}`
+                    })
+                })
         })
-        .catch(err=>{
+        .catch(err => {
             return res.status(500).json({
-                msg:`Can not reject this task with error: ${new Error(err.message)}`
+                msg: `Can not created remark with error: ${new Error(err.message)}`
             })
         })
-    })
-    .catch(err=>{
-        return res.status(500).json({
-            msg:`Can not created remark with error: ${new Error(err.message)}`
-        })
-    })
-   
+
 })
 
 
-router.get('/personal-tasks', authenticateDCToken,async (req, res) => {
+router.get('/personal-tasks', authenticateDCToken, async (req, res) => {
     let { page, search, status } = req.query;
     let stt = (status.split(',')).map(x => {
         return parseInt(x.trim());
@@ -264,7 +285,7 @@ router.get('/personal-tasks', authenticateDCToken,async (req, res) => {
 })
 
 
-router.get('/list', authenticateDCToken,async (req, res) => {
+router.get('/list', authenticateDCToken, async (req, res) => {
     let { page, search, status } = req.query;
     let stt = (status.split(',')).map(x => {
         return parseInt(x.trim());
@@ -272,7 +293,7 @@ router.get('/list', authenticateDCToken,async (req, res) => {
 
     let tasks = await Task
         .find({
-            status: { $in: stt }           
+            status: { $in: stt }
         })
         .sort({ 'deadline.end': 1 })
         .skip((page - 1) * pageSize)
@@ -289,9 +310,9 @@ router.get('/list', authenticateDCToken,async (req, res) => {
                 path: 'basic.level',
                 select: 'name'
             },
-            { path: 'editor.staff',select: 'fullname username' },
-            { path: 'qa.staff' ,select: 'fullname username'},
-            { path: 'dc.staff' ,select: 'fullname username'},
+            { path: 'editor.staff', select: 'fullname username' },
+            { path: 'qa.staff', select: 'fullname username' },
+            { path: 'dc.staff', select: 'fullname username' },
             { path: 'remarks' }
         ]);
 
