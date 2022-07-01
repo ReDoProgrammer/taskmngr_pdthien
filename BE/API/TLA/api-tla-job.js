666ffac43abc8a49413fc08d82509ed9af9ea0ef
ReDo Programmer <redoprogrammer@gmail.com>
2022-07-01 10:44:15 +0700

    load job detail with full populate

--- BE/API/TLA/api-tla-job.js
+++ BE/API/TLA/api-tla-job.js
@@ -29,8 +29,8 @@ router.get('/list', authenticateTLAToken, async (req, res) => {
             { path: 'templates.root' },
             { path: 'templates.parents' }
         ])
-        .sort({urgent:-1})
-        .sort({'deadline.end':1})
+        .sort({ urgent: -1 })
+        .sort({ 'deadline.end': 1 })
         .skip((page - 1) * pageSize)
         .limit(pageSize);
 
@@ -49,65 +49,66 @@ router.get('/list', authenticateTLAToken, async (req, res) => {
     })
 })
 
-router.get('/list-cc',authenticateTLAToken, async (req,res)=>{
-    let {jobId} = req.query;
+router.get('/list-cc', authenticateTLAToken, async (req, res) => {
+    let { jobId } = req.query;
     let job = await Job.findById(jobId)
-    .populate([
-        {path:'cc.root'},
-        {path:'cc.parents'}
-    ]);
-    if(!job){
+        .populate([
+            { path: 'cc.root' },
+            { path: 'cc.parents' }
+        ]);
+    if (!job) {
         return res.status(404).json({
-            msg:`Job not found so can not load CC list`
+            msg: `Job not found so can not load CC list`
         })
     }
     return res.status(200).json({
-        msg:`Load cc list based on job successfully!`,
+        msg: `Load cc list based on job successfully!`,
         cc: job.cc
     })
 
 })
 
-router.get('/list-tasks',authenticateTLAToken,async (req,res)=>{
-    let {jobId} = req.query;
+
+router.get('/list-tasks', authenticateTLAToken, async (req, res) => {
+    let { jobId } = req.query;
     let job = await Job.findById(jobId)
-    .populate([
-        {
-            path:'root.ref'  ,
-            select:'name'            
-        },
-        {
-            path:'root.tasks',
-            populate:[
-               { path:'basic.level'},
-               {path:'editor.staff'},
-               {path:'qa.staff'},
-               {path:'dc.staff'},
-               {path:'tla.uploaded.by'},
-            ]
-        },
-        {
-            path:'parents.ref'  ,
-            select:'name'            
-        },
-        {
-            path:'parents.tasks',
-            populate:[
-               { path:'basic.level'},
-               {path:'editor.staff'},
-               {path:'qa.staff'},
-               {path:'dc.staff'},
-               {path:'tla.uploaded.by'},
-            ]
-        }
-    ]);
-    if(!job){
+        .populate([
+            {
+                path: 'root.ref',
+                select: 'name'
+            },
+            {
+                path: 'root.tasks',
+                populate: [
+                    { path: 'basic.level' },
+                    { path: 'editor.staff' },
+                    { path: 'qa.staff' },
+                    { path: 'dc.staff' },
+                    { path: 'tla.uploaded.by' },
+                ]
+            },
+            {
+                path: 'parents.ref',
+                select: 'name'
+            },
+            {
+                path: 'parents.tasks',
+                populate: [
+                    { path: 'basic.level' },
+                    { path: 'editor.staff' },
+                    { path: 'qa.staff' },
+                    { path: 'dc.staff' },
+                    { path: 'tla.uploaded.by' },
+                ]
+            }
+        ]);
+    if (!job) {
         return res.status(404).json({
-            msg:`Can not list task belong to job cause job not found!`
+            msg: `Can not list task belong to job cause job not found!`
         })
     }
     return res.status(200).json({
-        msg:`List tasks based on job successfully!`,
+        msg: `List tasks based on job successfully!`,
         job
     })
 })
@@ -116,15 +117,31 @@ router.get('/detail', authenticateTLAToken, async (req, res) => {
 
     let { jobId } = req.query;
     let job = await Job.findById(jobId)
-    .populate('customer')
-    .populate({
-        path : 'cb',
-        populate : [
-            {path:'lines.root'},
-            {path:'lines.parents'}
-        ]
-      });
-     
+        .populate([
+            {path:'created.by', select:'username fullname'},
+            { path: 'customer' },
+            {
+                path: 'cb',
+                populate: ([
+                    { path: 'lines.root' },
+                    { path: 'lines.parents' }
+                ])
+            },
+            { path: 'cc.root' },
+            { path: 'cc.root.tasks' },
+            { path: 'cc.parents' },
+            { path: 'cc.parents.tasks' },
+            { path: 'captured.material' },
+            { path: 'captured.user' },
+            { path: 'templates.root' },
+            { path: 'templates.parents' },
+            { path: 'root.ref' },
+            { path: 'root.tasks' },
+            { path: 'parents.ref' },
+            { path: 'parents.tasks' }
+        ]);
+
+
     if (!job) {
         return res.status(404).json({
             msg: `Job not found!`
@@ -144,15 +161,15 @@ router.get('/local-level', authenticateTLAToken, async (req, res) => {
         let parents = await Parents.find({ _id: { $in: root.parents } })
 
         for (const p of parents) {
-            for (const l of p.job_levels) {                
-               if(!levels.includes(l)){
-                levels.push(l);
-               }
+            for (const l of p.job_levels) {
+                if (!levels.includes(l)) {
+                    levels.push(l);
+                }
             }
         }
 
-        let local_levels = await LocalLevel.find({_id:levels});
-       
+        let local_levels = await LocalLevel.find({ _id: levels });
+
         return res.status(200).json({
             msg: `Load local levels successfully!`,
             local_levels
